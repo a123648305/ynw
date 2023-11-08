f504ccd4e6ec3c8110a07253782b6ae45ec76156
wu suncoo <a123648305@126.com>
2023-11-08 20:48:49 +0800

    feat: 项目初始化 首页暂存

---
+++ gulpfile.js
@@ -0,0 +1,74 @@
+const { src, watch, dest, series } = require("gulp");
+const postcss = require("gulp-postcss");
+const gulpServer = require("gulp-webserver");
+const clean = require("gulp-clean");
+
+const cleanDist = function () {
+  return src("dist", { read: false }).pipe(clean());
+};
+
+// 处理静态资源
+const publicResource = function () {
+  src(["assets/img/*"]).pipe(dest("dist/img"));
+  src(["assets/font/*"]).pipe(dest("dist/font"));
+  return src(["lib/**/*"]).pipe(dest("dist/lib"));
+};
+
+//处理css
+const compileCss = function () {
+  return src(["assets/css/**/*.css"]).pipe(postcss()).pipe(dest("dist/css"));
+};
+
+//处理js
+const compileJs = function () {
+  return src(["src/js/**/*.js"]).pipe(dest("dist/js"));
+};
+
+// 处理html
+const compileHtml = function () {
+  return src("./src/pages/**/*.html").pipe(dest("dist/"));
+};
+
+// exports.default = cssTransfore;
+
+const devServer = function () {
+  return src("./dist/").pipe(
+    gulpServer({
+      port: 8888,
+      host: "localhost",
+      livereload: true, // 开启热更新
+      open: false, // 开启浏览器
+    })
+  );
+};
+
+// 监听文件
+const watchFile = function () {
+  watch("./assets/img/**/*", (cb) => {
+    publicResource();
+    cb();
+  });
+  watch("./assets/css/**/*.css", (cb) => {
+    compileCss();
+    cb();
+  });
+//   watch("./src/js/**/*.js", (cb) => {
+//     compileJs();
+//     cb();
+//   });
+  watch("./src/pages/**/*.html", (cb) => {
+    compileHtml();
+    cb();
+  });
+};
+
+exports.cssTransform = compileCss;
+exports.default = series(
+  cleanDist,
+  publicResource,
+  compileCss,
+  compileJs,
+  compileHtml,
+  devServer,
+  watchFile
+);
---
+++ readme.md
@@ -0,0 +1,20 @@
+## 开发手册
+### 目录简介
+--assets
+    |--images  // 存放图片资源
+    |--css  // 存放css文件
+    |--font  // 存放字体图标资源
+--src
+    |--pages //存放页面文件
+    |--js   // 存放js文件
+--lib      // 存放第三方插件 ui库
+    |--jq.js  // jq.js文件
+    |--bootstrap // bootstrap ui文件
+### 开发规范
++ 样式命名 
+    + 类名使用小写，以 - 分隔,加以前缀 ynw  例如 ynw-home
+
++ 响应式
+    + css 单位为px,可配合插件转为vw 单位
+    + 页面布局使用flex布局
+
