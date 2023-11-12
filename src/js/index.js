244f1625f0c289df1d2c88a651b3d56a85d1f30a
可乐plx <1689422585@qq.com>
2023-11-12 23:29:24 +0800

    feat: 首页调整

--- src/js/index.js
+++ src/js/index.js
@@ -7,12 +7,12 @@ window.onload = () => {
 // });
 
 $(document).ready(function () {
-  console.log("eeeeeeeee");
   heatScroll();
 });
 
 function heatScroll() {
-  const slider = $("#carouselTab");
+  var slider = $("#carouselTab");
+  var screenWidth=$('body').width()
   console.log("222222", slider);
   let startX = 0;
 
@@ -27,7 +27,7 @@ function heatScroll() {
     const data = e.originalEvent.touches[0];
     const { clientX } = data;
     console.log("touchmovvv", clientX);
-    $(this).css("transform", `translate3d(${clientX}, 0, 0)`);
+    $(this).css("transform", `translate3d(-${clientX}px, 0, 0)`);
   });
 
   slider.on("touchend", function (e) {
@@ -40,14 +40,33 @@ function heatScroll() {
   //     console.log("eee");
   //   });
 
-  var brand = $("#heat-touch a");
-  var activeEle;
-  Array.from(brand).forEach((element) => {
+  var brand = $("#heat-touch");
+  var brandItem=$("#heat-touch a");
+  var activeEle=$('#heat-touch .activate')
+  Array.from(brandItem).forEach((element) => {
     $(element).on("click", function (e) {
-      console.log("clicks", e);
+      var curTabLeft=$(this).offset().left
+      var curTabVal =$(this).data('value')
       $(activeEle).removeClass('activate');
-      $(element).addClass('activate');
-      activeEle=element
+      $(this).addClass('activate');
+      if(curTabLeft*2>screenWidth){
+        // 超过一半的位置 向前移动一点
+        var offsetL=curTabLeft-screenWidth/2
+        $(brand).css('transform','translateX(-'+offsetL+'px)')
+      }else{
+        $(brand).css('transform','translateX(0)')
+      }
+      activeEle=this
+
+      // 显示下面对应的面板
+      $('.ynw-home-heat-group>div').each(function (index,item) {
+        var itemVal=$(item).data('value')
+        if(itemVal===curTabVal){
+          $(item).removeClass('d-none')
+        }else{
+          $(item).addClass('d-none')
+        }
+      })
     });
   });
 }
