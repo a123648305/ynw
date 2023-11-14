d0c235be33d28141283381ddb8bf22080b916df4
wu suncoo <a123648305@126.com>
2023-11-14 13:43:04 +0800

    feat: 首页nav 优化

--- dist/js/home.js
+++ dist/js/home.js
@@ -5,83 +5,101 @@ window.onload = () => {
 // $(function() {
 // 	FastClick.attach(document.body);
 // });
+var screenWidth
 
 $(document).ready(function () {
-    heatScrollInit();
+  heatScrollInit();
+  brandTabInit();
+  screenWidth = $("body").width();
 });
-
 
 // tab 切换
 function heatScrollInit() {
   var slider = $("#carouselTab");
-  var naviDom =$('#navidot>i')
-  var screenWidth=$('body').width()
-
+  var naviDom = $("#navidot>i");
+  var tabWidth = $('#carouselTab>li').width()
+  var startX=0 // 开始滑动位置
+  var slideTabIndex=0   // 滑动的Index
+  var distanceX=0   // 计算偏移量
+  var isMove=false
   slider.on("touchstart", function (e) {
     var _touch = e.originalEvent.targetTouches[0];
-    var _x= _touch.pageX;
-    console.log("touchStart", _x);
-    $(this).css('transition','none')
+    startX=_touch.pageX;
+    console.log("touchStart", startX);
   });
 
   slider.on("touchmove", function (e) {
     var _touch = e.originalEvent.targetTouches[0];
-    var _x= _touch.pageX;
-    // console.log("touchmovvv", _x);
-    $(this).css("transform", `translate3d(${_x}px, 0, 0)`);
+    var _x = _touch.pageX;
+    distanceX = _x - startX;
+    var offsetX=-tabWidth * slideTabIndex+distanceX
+    console.log("touchmovvv", _x,distanceX,offsetX);
+    isMove=true
+    setTranslateX(offsetX)
   });
 
   slider.on("touchend", function (e) {
-    var _touch = e.originalEvent.changedTouches[0];
-    var _x= _touch.pageX;
-    var _showIndex
-    if(_x>0){
-        $(this).css('transition','all 0.3s ease').css("transform", `translate3d(0, 0, 0)`)
-        _showIndex=1
-    }else{
-        $(this).css('transition','all 0.3s ease').css("transform", `translate3d(-50%, 0, 0)`)
-        _showIndex=2
-    }
-    // console.log("touchend", _x);
-
-
-    $(naviDom).each(function(i,ele){
-        if($(ele).data('index')===_showIndex){
-            $(ele).addClass('activate')
-        }else{
-            $(ele).removeClass('activate')
+    if (isMove) {
+        var _touch = e.originalEvent.changedTouches[0];
+        var _x = _touch.pageX;
+        console.log("touchmoend",naviDom.length, _x);
+        if (Math.abs(distanceX) >= tabWidth / 3) {
+            if (distanceX > 0&&slideTabIndex>0) {
+                slideTabIndex--;
+            }
+            if (distanceX < 0&&slideTabIndex<naviDom.length-1) {
+                slideTabIndex++;
+            }
         }
-    })
+        setTranslateX(-slideTabIndex * tabWidth);
+    }
+    startX = 0;
+    distanceX = 0;
+    isMove = false;
+
+    $(naviDom).each(function (i, ele) {
+      if ($(ele).data("index") === slideTabIndex) {
+        $(ele).addClass("activate");
+      } else {
+        $(ele).removeClass("activate");
+      }
+    });
   });
 
-  // 品牌切换
+  function setTranslateX(value){
+    $(slider).css("transform", 'translate3d('+value+'px, 0, 0)')
+  }
+}
+
+// 品牌切换
+function brandTabInit() {
   var brand = $("#heat-touch");
-  var brandItem=$("#heat-touch a");
-  var activeEle=$('#heat-touch .activate')
+  var brandItem = $("#heat-touch a");
+  var activeEle = $("#heat-touch .activate");
   Array.from(brandItem).forEach((element) => {
     $(element).on("click", function (e) {
-      var curTabLeft=$(this).offset().left
-      var curTabVal =$(this).data('value')
-      $(activeEle).removeClass('activate');
-      $(this).addClass('activate');
-      if(curTabLeft*2>screenWidth){
+      var curTabLeft = $(this).offset().left;
+      var curTabVal = $(this).data("value");
+      $(activeEle).removeClass("activate");
+      $(this).addClass("activate");
+      if (curTabLeft * 2 > screenWidth) {
         // 超过一半的位置 向前移动一点
-        var offsetL=curTabLeft-screenWidth/2
-        $(brand).css('transform','translateX(-'+offsetL+'px)')
-      }else{
-        $(brand).css('transform','translateX(0)')
+        var offsetL = curTabLeft - screenWidth / 2;
+        $(brand).css("transform", "translateX(-" + offsetL + "px)");
+      } else {
+        $(brand).css("transform", "translateX(0)");
       }
-      activeEle=this
+      activeEle = this;
 
       // 显示下面对应的面板
-      $('.ynw-home-heat-group>div').each(function (index,item) {
-        var itemVal=$(item).data('value')
-        if(itemVal===curTabVal){
-          $(item).removeClass('d-none')
-        }else{
-          $(item).addClass('d-none')
+      $(".ynw-home-heat-group>div").each(function (index, item) {
+        var itemVal = $(item).data("value");
+        if (itemVal === curTabVal) {
+          $(item).removeClass("d-none");
+        } else {
+          $(item).addClass("d-none");
         }
-      })
+      });
     });
   });
 }
