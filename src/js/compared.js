5dadee699d834c2998b1987c3e3986d4d0f00211
wu suncoo <a123648305@126.com>
2023-11-14 17:46:52 +0800

    feat: 比较页面优化

--- src/js/compared.js
+++ src/js/compared.js
@@ -10,25 +10,44 @@ function tableSlideInit() {
   $(".differ-main").on("touchmove", function (e) {
     var _touch = e.originalEvent.changedTouches[0];
     var _x = _touch.pageX;
-    console.log(_x, "xxx");
-    $(".differ-main").scrollLeft($(this).scrollLeft())
-    $(".console-main").scrollLeft($(this).scrollLeft())
+    // console.log(_x, "xxx");
+    // $(".differ-main").scrollLeft($(this).scrollLeft());
+    $(".console-main").scrollLeft($(this).scrollLeft());
+    var _this = this;
+    $(".differ-main").each(function (i, item) {
+      if ($(item) !== $(_this)) {
+        $(item).scrollLeft($(_this).scrollLeft());
+      } 
+    });
   });
 
   $(".console-main").on("touchmove", function (e) {
     var _touch = e.originalEvent.changedTouches[0];
     var _x = _touch.pageX;
-    console.log(_x, "xxx");
-    $(".differ-main").scrollLeft($(this).scrollLeft())
+    // console.log(_x, "xxx");
+    $(".differ-main").scrollLeft($(this).scrollLeft());
   });
-
 
   //   $(".differ-main").on("touchend", function (e) {});
 }
 
+// 移除比较项
+function removeCard(e) {
+  var remParent = $(e.target).parent();
+  var removeIndex = remParent.data("index");
+  console.log(e, "ee", removeIndex);
+  remParent.remove();
+  $(".differ-main .inner").each(function (i, item) {
+    if ($(item).data("index") === removeIndex) {
+      $(item).remove();
+    }
+  });
+}
+
 function test() {
   $(".differ-main").css("border", "1px solid red");
-  $(".differ-main").animate({
-    scrollLeft: "600px",
-  });
+  //   $(".differ-main").animate({
+  //     scrollLeft: "600px",
+  //   });
+  $(".differ-main").scrollLeft(500);
 }
