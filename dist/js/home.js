e79054676ba74c71981403ee2b21ee9d4785f01d
wu suncoo <a123648305@126.com>
2023-11-15 11:04:36 +0800

    feat: 首页优化

--- dist/js/home.js
+++ dist/js/home.js
@@ -5,7 +5,7 @@ window.onload = () => {
 // $(function() {
 // 	FastClick.attach(document.body);
 // });
-var screenWidth
+var screenWidth;
 
 $(document).ready(function () {
   heatScrollInit();
@@ -17,36 +17,36 @@ $(document).ready(function () {
 function heatScrollInit() {
   var slider = $("#carouselTab");
   var naviDom = $("#navidot>i");
-  var tabWidth = $('#carouselTab>li').width()
-  var startX=0 // 开始滑动位置
-  var slideTabIndex=0   // 滑动的Index
-  var distanceX=0   // 计算偏移量
-  var isMove=false
+  var tabWidth = $("#carouselTab>li").width();
+  var startX = 0; // 开始滑动位置
+  var slideTabIndex = 0; // 滑动的Index
+  var distanceX = 0; // 计算偏移量
+  var isMove = false;
   slider.on("touchstart", function (e) {
     var _touch = e.originalEvent.targetTouches[0];
-    startX=_touch.pageX;
+    startX = _touch.pageX;
   });
 
   slider.on("touchmove", function (e) {
     var _touch = e.originalEvent.targetTouches[0];
     var _x = _touch.pageX;
     distanceX = _x - startX;
-    var offsetX=-tabWidth * slideTabIndex+distanceX
-    isMove=true
-    setTranslateX(offsetX)
+    var offsetX = -tabWidth * slideTabIndex + distanceX;
+    isMove = true;
+    setTranslateX(offsetX);
   });
 
   slider.on("touchend", function (e) {
     if (isMove) {
-        if (Math.abs(distanceX) >= tabWidth / 3) {
-            if (distanceX > 0&&slideTabIndex>0) {
-                slideTabIndex--;
-            }
-            if (distanceX < 0&&slideTabIndex<naviDom.length-1) {
-                slideTabIndex++;
-            }
+      if (Math.abs(distanceX) >= tabWidth / 3) {
+        if (distanceX > 0 && slideTabIndex > 0) {
+          slideTabIndex--;
         }
-        setTranslateX(-slideTabIndex * tabWidth);
+        if (distanceX < 0 && slideTabIndex < naviDom.length - 1) {
+          slideTabIndex++;
+        }
+      }
+      setTranslateX(-slideTabIndex * tabWidth);
     }
     startX = 0;
     distanceX = 0;
@@ -61,8 +61,8 @@ function heatScrollInit() {
     });
   });
 
-  function setTranslateX(value){
-    $(slider).css("transform", 'translate3d('+value+'px, 0, 0)')
+  function setTranslateX(value) {
+    $(slider).css("transform", "translate3d(" + value + "px, 0, 0)");
   }
 }
 
@@ -77,13 +77,9 @@ function brandTabInit() {
       var curTabVal = $(this).data("value");
       $(activeEle).removeClass("activate");
       $(this).addClass("activate");
-      if (curTabLeft * 2 > screenWidth) {
-        // 超过一半的位置 向前移动一点
-        var offsetL = curTabLeft - screenWidth / 2;
-        $(brand).css("transform", "translateX(-" + offsetL + "px)");
-      } else {
-        $(brand).css("transform", "translateX(0)");
-      }
+      // 超过一半的位置 向前移动一点
+      var offsetL = offsetL = curTabLeft - screenWidth / 2
+      $(brand).animate({scrollLeft:offsetL})
       activeEle = this;
 
       // 显示下面对应的面板
