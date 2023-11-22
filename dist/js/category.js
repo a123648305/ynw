4f7e23e2927044c503f74c085ade64df2555bf68
wu suncoo <a123648305@126.com>
2023-11-22 15:18:53 +0800

    feat: 暂存

--- dist/js/category.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  var titile = new URLSearchParams(location.search).get("title");
-  $(".ynw-category-total>h1").text(titile);
-});
-
-function toDetail(e) {
-  var value = $(e.currentTarget).data("id");
-  window.location.href = "detail.html?id=" + value;
-}
