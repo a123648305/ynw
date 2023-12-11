f19e3fc6969f09ed4d9c85f9c43ce7a5ffeebdad
wu suncoo <a123648305@126.com>
2023-12-11 18:14:32 +0800

    feat: 详情暂存

--- src/js/search.js
+++ src/js/search.js
@@ -1,9 +1,31 @@
 $(function () {
   console.log("cccccc");
-  sortDropInit();
-  brandInit();
-  adScrollInit();
 });
+
+// 搜索关键词
+function searchKeyword() {
+  var keyword = $("#search").val();
+  console.log(keyword);
+
+  $(".ynw-official-search-result").show();
+  $(".ynw-official-search-detail-hot").show();
+}
+
+function quickSearch(e) {
+  var quickKeyword = $(e.target).data("letter");
+
+  if (quickKeyword) {
+    $(".ynw-official-search-result").show();
+    $(".ynw-official-search-detail-hot").show();
+  }
+  console.log(quickKeyword);
+}
+
+function resetKeyword() {
+  $("#search").val("");
+  $(".ynw-official-search-result").hide();
+  $(".ynw-official-search-detail-hot").hide();
+}
 
 // 列表点击
 function resultClcik() {
@@ -12,7 +34,28 @@ function resultClcik() {
 
 // tab  切换
 function tabChange(e) {
-  console.log("eee", e);
+  var index = $(e.target).data("value");
+  var ticks = $("#ticks");
+  var information = $("#information");
+
+  $(".nav-tab-item").each(function (i, item) {
+    if ($(item).data("value") === index) {
+      $(item).addClass("active");
+    } else {
+      $(item).removeClass("active");
+    }
+  });
+
+  if (index === "ticks") {
+    ticks.show();
+    information.hide();
+  }
+  if (index === "information") {
+    ticks.hide();
+    information.show();
+  }
+  if (index === "all") {
+    ticks.show();
+    information.show();
+  }
 }
-
-
